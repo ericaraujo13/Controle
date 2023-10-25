31707a7ea632cc1d15750d677fda2e3964654433
ericaruajo13 <ericpoloni@gmail.com>
2023-10-25 19:20:13 -0300

    fix

--- db/migrate/20231025221024_add_user_type_to_user.rb
+++ db/migrate/20231025221024_add_user_type_to_user.rb
@@ -1,5 +1,5 @@
 class AddUserTypeToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :user_type, :integer, default: 0
+    add_column :users, :user_type, :integer, default: 1
   end
 end
