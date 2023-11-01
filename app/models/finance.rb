6c90afe364bb5f18c61027bda56f0b87bf55c5b4
Eric Araujo <ericaraujo13>
2023-11-01 18:09:33 -0300

    add validations

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,5 +2,7 @@ class Finance < ApplicationRecord
   belongs_to :user
   
   validates :description, presence: true
+  validates :transaction_type, presence: true
+  validates :amount, presence: true
   enum transaction_type: [ :entrada, :saida ]
 end
